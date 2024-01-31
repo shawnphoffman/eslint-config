1e46f77138f5a507b7f8223829c9dd72dcbf941b
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-31 09:03:58 -0800

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,23 @@
 module.exports = {
 	env: { es6: true },
 	extends: ['airbnb-base', 'prettier'],
+	plugins: ['simple-import-sort'],
 	parserOptions: { ecmaVersion: 'latest' },
-};
+	rules: {
+		'simple-import-sort/imports': [
+			'error',
+			{
+				groups: [
+					['^\\u0000'],
+					['^react$', '^react', '^@?\\w'],
+					['^(@root|types|data)(/.*)'],
+					['^(app|components|utils|context|config|pages|hooks|styles|public)(/.*)'],
+					[('^\\.\\.(?!/?$)', '^\\.\\./?$')],
+					['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+				],
+			},
+		],
+		'sort-imports': 'off',
+		'import/order': 'off',
+	},
+}
